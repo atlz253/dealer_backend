983ac05c7a3f7f8d2ab5dfaf9a93d7db05b610b7
Fedor Alekseev <f.alexeeff2010@yandex.ru>
2023-04-14 22:36:43 +0300

    init

---
+++ index.ts
@@ -0,0 +1,48 @@
+import express, { Express, Request, Response } from "express";
+import IContract from "./node_modules/audio_diler_common/interfaces/IContract";
+
+const app: Express = express();
+const port = 2727
+
+app.get('/', (req: Request, res: Response) => {
+  res.send("Hello world");
+});
+
+app.get('/contracts', (req: Request, res: Response) => {
+  const mockup: IContract[] = [
+    {
+      id: 0,
+      seller: "Столяров Д.Д.",
+      buyer: "Мигас А.С.",
+      price: 999,
+      date: Date.now()
+    },
+    {
+      id: 1,
+      seller: "Столяров Д.Д.",
+      buyer: "Мигас А.С.",
+      price: 999,
+      date: Date.now()
+    },
+    {
+      id: 2,
+      seller: "Столяров Д.Д.",
+      buyer: "Мигас А.С.",
+      price: 9999,
+      date: Date.now()
+    },
+    {
+      id: 3,
+      seller: "Столяров Д.Д.",
+      buyer: "Мигас А.С.",
+      price: 999,
+      date: Date.now()
+    },
+  ];
+
+  res.send(mockup);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
