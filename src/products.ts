fbc9ec099f611a514f8ebd598eedf879529ed02c
Фёдор Алексеев <f.alexeeff2010@yandex.ru>
2023-04-18 16:39:39 +0300

    Начал реализовывать получение категории по ID

--- src/products.ts
+++ src/products.ts
@@ -52,10 +52,39 @@ productsRouter.get('/:productID', (req: Request, res: Response) => {
     });
 });
 
-productsRouter.post("/new", (req: Request, res: Response) => {
-    pool.query(`INSERT INTO "Products" ("name", "category", "price", "quantity", "description") VALUES ('${req.body.name}', '${req.body.category}', '${req.body.price}', '${req.body.quantity}', '${req.body.description}') RETURNING "productID" as "id"`, (error, result) => {
+productsRouter.post("/new", async (req: Request, res: Response) => {
+    let productID: any;
+
+    try {
+        const result = await pool.query(`SELECT "categoryID" as "id" FROM "Categories" WHERE "name" = '${req.body.category}'`);
+
+        console.log(result.rows);
+    } catch (error) {
+        console.log(error);
+        
+    }
+
+    // if (productID === undefined) {
+    //     pool.query(`INSERT INTO "Categories" ("name") VALUES ('${req.body.category}') RETURNING "categoryID" as "id"`, (error, result) => {
+    //         if (error) {
+    //             console.error(error);
+
+    //             const response: IResponse = {
+    //                 status: 400
+    //             }
+
+    //             return res.json(response);
+    //         }
+
+    //         productID = result.rows[0].id;
+    //     });
+    // }
+
+    console.log(productID);
+
+    pool.query(`INSERT INTO "Products" ("name", "price", "quantity", "description") VALUES ('${req.body.name}', '${req.body.price}', '${req.body.quantity}', '${req.body.description}') RETURNING "productID" as "id"`, (error, result) => {
         if (error) {
-            console.error(error);
+            // console.error(error);
 
             const response: IResponse = {
                 status: 400
